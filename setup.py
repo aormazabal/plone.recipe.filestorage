29d78e0da321c0cd01a2efaca087c3c68f209ade
Albert Ormazabal Blanc <aormazabal@tda.ad>
2022-11-09 10:19:42 +0100

    change authors

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(*rnames):
     return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
 
 
-version = '0.7.2'
+version = '0.7.3'
 
 long_description = (
         'Detailed Documentation\n'
